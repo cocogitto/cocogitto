109869b8e0fd3a28263dcfa6541eab4ddfd2a0ee
Paul Delafosse <paul.delafosse@protonmail.com>
2020-10-10 12:47:36 +0200

    fix: cog bump now perform a single version bump (#44)

--- tests/cog_bump_test.rs
+++ tests/cog_bump_test.rs
@@ -27,7 +27,7 @@ fn auto_bump_from_start_ok() -> Result<()> {
 
 #[test]
 #[cfg(not(tarpaulin))]
-fn auto_bump_frowm_latest_tag() -> Result<()> {
+fn auto_bump_minor_from_latest_tag() -> Result<()> {
     let current_dir = std::env::current_dir()?;
     let mut command = Command::cargo_bin("cog")?;
     command.arg("bump").arg("--auto");
@@ -45,7 +45,57 @@ fn auto_bump_frowm_latest_tag() -> Result<()> {
 
     command.assert().success();
     assert!(temp_dir.join("CHANGELOG.md").exists());
-    helper::assert_tag("1.3.0")?;
+    helper::assert_tag("1.1.0")?;
+
+    Ok(std::env::set_current_dir(current_dir)?)
+}
+
+#[test]
+#[cfg(not(tarpaulin))]
+fn auto_bump_major_from_latest_tag() -> Result<()> {
+    let current_dir = std::env::current_dir()?;
+    let mut command = Command::cargo_bin("cog")?;
+    command.arg("bump").arg("--auto");
+
+    let temp_dir = TempDir::default();
+    std::env::set_current_dir(&temp_dir)?;
+    helper::git_init(".")?;
+    helper::git_commit("chore: init")?;
+    helper::git_commit("feat(taef): feature")?;
+    helper::git_commit("fix: bug fix")?;
+    helper::git_tag("1.0.0")?;
+    helper::git_commit("feat!(taef): feature")?;
+    helper::git_commit("feat!: feature 1")?;
+    helper::git_commit("feat: feature 2")?;
+
+    command.assert().success();
+    assert!(temp_dir.join("CHANGELOG.md").exists());
+    helper::assert_tag("2.0.0")?;
+
+    Ok(std::env::set_current_dir(current_dir)?)
+}
+
+#[test]
+#[cfg(not(tarpaulin))]
+fn auto_bump_patch_from_latest_tag() -> Result<()> {
+    let current_dir = std::env::current_dir()?;
+    let mut command = Command::cargo_bin("cog")?;
+    command.arg("bump").arg("--auto");
+
+    let temp_dir = TempDir::default();
+    std::env::set_current_dir(&temp_dir)?;
+    helper::git_init(".")?;
+    helper::git_commit("chore: init")?;
+    helper::git_commit("feat(taef): feature")?;
+    helper::git_commit("fix: bug fix")?;
+    helper::git_tag("1.0.0")?;
+    helper::git_commit("fix(the_fix): the_fix")?;
+    helper::git_commit("fix: fix 1")?;
+    helper::git_commit("fix: fix 2")?;
+
+    command.assert().success();
+    assert!(temp_dir.join("CHANGELOG.md").exists());
+    helper::assert_tag("1.0.1")?;
 
     Ok(std::env::set_current_dir(current_dir)?)
 }
