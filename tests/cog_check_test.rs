78815c243f95c38371cc5157e27bd7cba50c72a4
Paul Delafosse <paul.delafosse@protonmail.com>
2021-09-16 10:53:08 +0200

    test: refactor IT test using a run in context helper

--- tests/cog_check_test.rs
+++ tests/cog_check_test.rs
@@ -5,47 +5,46 @@ use predicates::prelude::predicate;
 mod helper;
 use helper::run_test_with_context;
 
-
 #[test]
 #[cfg(not(tarpaulin))]
 fn cog_check_ok() -> Result<()> {
-  run_test_with_context(|_| {
-      // Arrange
-      helper::git_init(".")?;
-      helper::git_commit("chore: init")?;
-      helper::git_commit("feat: feature")?;
-      helper::git_commit("fix: bug fix")?;
+    run_test_with_context(|_| {
+        // Arrange
+        helper::git_init()?;
+        helper::git_commit("chore: init")?;
+        helper::git_commit("feat: feature")?;
+        helper::git_commit("fix: bug fix")?;
 
-      // Act
-      Command::cargo_bin("cog")?
-          .arg("check")
-        // Assert
-          .assert()
-          .success()
-          .stdout(predicate::str::contains("No errored commits"));
-      Ok(())
-  })
+        // Act
+        Command::cargo_bin("cog")?
+            .arg("check")
+            // Assert
+            .assert()
+            .success()
+            .stdout(predicate::str::contains("No errored commits"));
+        Ok(())
+    })
 }
 
 #[test]
 #[cfg(not(tarpaulin))]
 fn cog_check_failure() -> Result<()> {
-   run_test_with_context(|_| {
-       // Arrange
-       helper::git_init(".")?;
-       helper::git_commit("chore: init")?;
-       helper::git_commit("toto: feature")?;
-       helper::git_commit("fix: bug fix")?;
+    run_test_with_context(|_| {
+        // Arrange
+        helper::git_init()?;
+        helper::git_commit("chore: init")?;
+        helper::git_commit("toto: feature")?;
+        helper::git_commit("fix: bug fix")?;
 
-       // Act
-       Command::cargo_bin("cog")?
-           .arg("check")
-       // Assert
-           .assert()
-           .failure()
-           .stderr(predicate::str::contains("Commit type `toto` not allowed"));
-       Ok(())
-   })
+        // Act
+        Command::cargo_bin("cog")?
+            .arg("check")
+            // Assert
+            .assert()
+            .failure()
+            .stderr(predicate::str::contains("Commit type `toto` not allowed"));
+        Ok(())
+    })
 }
 
 #[test]
@@ -53,7 +52,7 @@ fn cog_check_failure() -> Result<()> {
 fn cog_check_from_latest_tag_ok() -> Result<()> {
     run_test_with_context(|_| {
         // Arrange
-        helper::git_init(".")?;
+        helper::git_init()?;
         helper::git_commit("chore: init")?;
         helper::git_commit("toto: errored commit")?;
         helper::git_commit("feat: feature")?;
@@ -77,7 +76,7 @@ fn cog_check_from_latest_tag_ok() -> Result<()> {
 fn cog_check_from_latest_tag_failure() -> Result<()> {
     run_test_with_context(|_| {
         // Arrange
-        helper::git_init(".")?;
+        helper::git_init()?;
         helper::git_commit("chore: init")?;
         helper::git_commit("toto: errored commit")?;
         helper::git_commit("feat: feature")?;
