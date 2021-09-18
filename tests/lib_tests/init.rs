7beec9fda262dcae8b6fada61d0b0e8ec94883e2
Paul Delafosse <paul.delafosse@protonmail.com>
2021-09-18 19:04:37 +0200

    test: add init tests

---
+++ tests/lib_tests/init.rs
@@ -0,0 +1,38 @@
+use anyhow::Result;
+
+use crate::helpers::*;
+
+use speculoos::prelude::*;
+use std::path::Path;
+
+#[test]
+fn should_init_a_cog_repository() -> Result<()> {
+    // Arrange
+    run_test_with_context(|_| {
+        // Act
+        cocogitto::init(".")?;
+
+        // Assert
+        assert_that(&Path::new("cog.toml")).exists();
+        assert_that(&git_log_head()?).is_equal_to("chore: initial commit\n".to_string());
+        Ok(())
+    })
+}
+
+#[test]
+fn should_skip_initialization_if_repository_exists() -> Result<()> {
+    // Arrange
+    run_test_with_context(|_| {
+        git_init()?;
+        git_commit("The first commit")?;
+
+        // Act
+        cocogitto::init(".")?;
+
+        // Assert
+        assert_that(&Path::new("cog.toml")).exists();
+        assert_that(&git_log_head()?).is_equal_to("The first commit\n\n".to_string());
+        assert_that(&git_status()?).contains("new file:   cog.toml");
+        Ok(())
+    })
+}
