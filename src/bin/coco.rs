ccd08d004e5730cf3604c65911e9930f66e53030
Marcin Puc <marcin.e.puc@gmail.com>
2022-01-02 23:25:05 +0100

    refactor: switch arg parsing to clap v3

--- src/bin/coco.rs
+++ src/bin/coco.rs
@@ -4,52 +4,51 @@ mod cog_commit;
 use cocogitto::CocoGitto;
 
 use anyhow::Result;
-use structopt::clap::{AppSettings, Shell};
-use structopt::StructOpt;
-
-const APP_SETTINGS: &[AppSettings] = &[
-    AppSettings::UnifiedHelpMessage,
-    AppSettings::ColoredHelp,
-    AppSettings::DeriveDisplayOrder,
-];
+use clap::{AppSettings, IntoApp, Parser};
+use clap_complete::Shell;
 
 /// A command line tool to create conventional commits
 ///
 /// Deprecated in favor of the `cog commit` utility
-#[derive(StructOpt)]
-#[structopt(name = "Coco", author = "Paul D. <paul.delafosse@protonmail.com>", settings = APP_SETTINGS)]
+#[derive(Parser)]
+#[clap(setting = AppSettings::DeriveDisplayOrder)]
+#[clap(
+    version,
+    name = "Coco",
+    author = "Paul D. <paul.delafosse@protonmail.com>"
+)]
 struct Cli {
     /// Conventional commit type
-    #[structopt(name = "type", possible_values = &cog_commit::commit_types(), default_value_if("completion", None, "chore"))]
+    #[clap(name = "type", value_name = "TYPE", possible_values = cog_commit::commit_types(), default_value_if("completion", None, Some("chore")))]
     typ: String,
 
     /// Commit description
-    #[structopt(default_value_if("completion", None, ""))]
+    #[clap(default_value_if("completion", None, Some("")))]
     message: String,
 
     /// Conventional commit scope
     scope: Option<String>,
 
     /// Create a BREAKING CHANGE commit
-    #[structopt(short = "B", long)]
+    #[clap(short = 'B', long)]
     breaking_change: bool,
 
     /// Open commit message in an editor
-    #[structopt(short, long)]
+    #[clap(short, long)]
     edit: bool,
 
     /// Generate shell completions
-    #[structopt(long, conflicts_with = "type")]
+    #[clap(long, conflicts_with = "type")]
     completion: Option<Shell>,
 }
 
 fn main() -> Result<()> {
-    let cli = Cli::from_args();
+    let cli = Cli::parse();
 
     eprintln!("Warning: `coco` is deprecated, use `cog commit` instead");
 
     if let Some(shell) = cli.completion {
-        Cli::clap().gen_completions_to("coco", shell, &mut std::io::stdout());
+        clap_complete::generate(shell, &mut Cli::into_app(), "coco", &mut std::io::stdout());
     } else {
         let cocogitto = CocoGitto::get()?;
 
--- src/bin/cog.rs
+++ src/bin/cog.rs
@@ -7,28 +7,14 @@ use cocogitto::conventional::changelog::template::{RemoteContext, Template};
 use cocogitto::conventional::commit;
 use cocogitto::conventional::version::VersionIncrement;
 use cocogitto::git::hook::HookKind;
+use cocogitto::git::revspec::RevspecPattern;
 use cocogitto::log::filter::{CommitFilter, CommitFilters};
 use cocogitto::log::output::Output;
 use cocogitto::{CocoGitto, SETTINGS};
 
 use anyhow::{Context, Result};
-use cocogitto::git::revspec::RevspecPattern;
-use structopt::clap::{AppSettings, Shell};
-use structopt::StructOpt;
-
-const APP_SETTINGS: &[AppSettings] = &[
-    AppSettings::SubcommandRequiredElseHelp,
-    AppSettings::UnifiedHelpMessage,
-    AppSettings::ColoredHelp,
-    AppSettings::VersionlessSubcommands,
-    AppSettings::DeriveDisplayOrder,
-];
-
-const SUBCOMMAND_SETTINGS: &[AppSettings] = &[
-    AppSettings::UnifiedHelpMessage,
-    AppSettings::ColoredHelp,
-    AppSettings::DeriveDisplayOrder,
-];
+use clap::{AppSettings, Args, IntoApp, Parser};
+use clap_complete::Shell;
 
 fn hook_profiles() -> Vec<&'static str> {
     SETTINGS
@@ -39,14 +25,18 @@ fn hook_profiles() -> Vec<&'static str> {
 }
 
 /// A command line tool for the conventional commits and semver specifications
-#[derive(StructOpt)]
-#[structopt(name = "Cog", author = "Paul D. <paul.delafosse@protonmail.com>", settings = APP_SETTINGS)]
+#[derive(Parser)]
+#[clap(global_setting = AppSettings::DeriveDisplayOrder)]
+#[clap(
+    version,
+    name = "Cog",
+    author = "Paul D. <paul.delafosse@protonmail.com>"
+)]
 enum Cli {
     /// Verify all commit messages against the conventional commit specification
-    #[structopt(no_version, settings = SUBCOMMAND_SETTINGS)]
     Check {
         /// Check commit history, starting from the latest tag to HEAD
-        #[structopt(short = "l", long)]
+        #[clap(short = 'l', long)]
         from_latest_tag: bool,
     },
 
@@ -54,136 +44,127 @@ enum Cli {
     Commit(CommitArgs),
 
     /// Interactively rename invalid commit messages
-    #[structopt(no_version, settings = SUBCOMMAND_SETTINGS)]
     Edit {
         /// Edit non conventional commits, starting from the latest tag to HEAD
-        #[structopt(short = "l", long)]
+        #[clap(short = 'l', long)]
         from_latest_tag: bool,
     },
 
     /// Like git log but for conventional commits
-    #[structopt(no_version, settings = SUBCOMMAND_SETTINGS)]
     Log {
         /// filter BREAKING CHANGE commits
-        #[structopt(short = "B", long)]
+        #[clap(short = 'B', long)]
         breaking_change: bool,
 
         /// filter on commit type
-        #[structopt(short, long = "type", value_name = "type")]
+        #[clap(short, long = "type", value_name = "type")]
         typ: Option<Vec<String>>,
 
         /// filter on commit author
-        #[structopt(short, long)]
+        #[clap(short, long)]
         author: Option<Vec<String>>,
 
         /// filter on commit scope
-        #[structopt(short, long)]
+        #[clap(short, long)]
         scope: Option<Vec<String>>,
 
         /// omit error on the commit log
-        #[structopt(short = "e", long)]
+        #[clap(short = 'e', long)]
         no_error: bool,
     },
 
     /// Verify a single commit message
-    #[structopt(no_version, settings = SUBCOMMAND_SETTINGS)]
     Verify {
         /// The commit message
         message: String,
     },
 
     /// Display a changelog for the given commit oid range
-    #[structopt(no_version, settings = SUBCOMMAND_SETTINGS)]
     Changelog {
         /// Generate the changelog from in the given spec range
-        #[structopt(conflicts_with = "at")]
+        #[clap(conflicts_with = "at")]
         pattern: Option<String>,
 
         /// Generate the changelog for a specific git tag
-        #[structopt(short, long)]
+        #[clap(short, long)]
         at: Option<String>,
 
         /// Generate the changelog with the given template.
         /// Possible values are 'remote', 'full_hash', 'default' or the path to your template.  
         /// If not specified cog will use cog.toml template config or fallback to 'default'.
-        #[structopt(name = "template", long, short)]
+        #[clap(name = "template", long, short)]
         template: Option<String>,
 
         /// Url to use during template generation
-        #[structopt(name = "remote", long, short, required_if("template", "remote"))]
+        #[clap(name = "remote", long, short, required_if_eq("template", "remote"))]
         remote: Option<String>,
 
         /// Repository owner to use during template generation
-        #[structopt(name = "owner", long, short, required_if("template", "remote"))]
+        #[clap(name = "owner", long, short, required_if_eq("template", "remote"))]
         owner: Option<String>,
 
         /// Name of the repository used during template generation
-        #[structopt(name = "repository", long, required_if("template", "remote"))]
+        #[clap(name = "repository", long, required_if_eq("template", "remote"))]
         repository: Option<String>,
     },
 
     /// Commit changelog from latest tag to HEAD and create new tag
-    #[structopt(no_version, settings = SUBCOMMAND_SETTINGS)]
     Bump {
         /// Manually set the next version
-        #[structopt(short, long, required_unless_one = &["auto", "major", "minor", "patch"])]
+        #[clap(short, long, required_unless_present_any = ["auto", "major", "minor", "patch"])]
         version: Option<String>,
 
         /// Automatically suggest the next version
-        #[structopt(short, long, required_unless_one = &["version", "major", "minor", "patch"])]
+        #[clap(short, long, required_unless_present_any = ["version", "major", "minor", "patch"])]
         auto: bool,
 
         /// Increment the major version
-        #[structopt(short = "M", long, required_unless_one = &["version", "auto", "minor", "patch"])]
+        #[clap(short = 'M', long, required_unless_present_any = ["version", "auto", "minor", "patch"])]
         major: bool,
 
         /// Increment the minor version
-        #[structopt(short, long, required_unless_one = &["version", "auto", "major", "patch"])]
+        #[clap(short, long, required_unless_present_any = ["version", "auto", "major", "patch"])]
         minor: bool,
 
         /// Increment the patch version
-        #[structopt(short, long, required_unless_one = &["version", "auto", "major", "minor"])]
+        #[clap(short, long, required_unless_present_any = ["version", "auto", "major", "minor"])]
         patch: bool,
 
         /// Set the pre-release version
-        #[structopt(long)]
+        #[clap(long)]
         pre: Option<String>,
 
         /// Specify the bump profile hooks to run
-        #[structopt(short, long, possible_values = &hook_profiles())]
+        #[clap(short = 'H', long, possible_values = hook_profiles())]
         hook_profile: Option<String>,
     },
 
     /// Install cog config files
-    #[structopt(no_version, settings = SUBCOMMAND_SETTINGS)]
     Init {
         /// path to init
-        #[structopt(default_value = ".")]
+        #[clap(default_value = ".")]
         path: PathBuf,
     },
 
     /// Add git hooks to the repository
-    #[structopt(no_version, settings = SUBCOMMAND_SETTINGS)]
     InstallHook {
         /// Type of hook to install
-        #[structopt(possible_values = &["commit-msg", "pre-push", "all"])]
+        #[clap(possible_values = &["commit-msg", "pre-push", "all"])]
         hook_type: String,
     },
 
     /// Generate shell completions
-    #[structopt(no_version, settings = SUBCOMMAND_SETTINGS)]
     GenerateCompletions {
         /// Type of completions to generate
-        #[structopt(name = "type", possible_values = &["bash", "elvish", "fish", "zsh"])]
+        #[clap(name = "type", arg_enum)]
         shell: Shell,
     },
 }
 
-#[derive(StructOpt)]
-#[structopt(flatten)]
+#[derive(Args)]
 struct CommitArgs {
     /// Conventional commit type
-    #[structopt(name = "type", possible_values = &cog_commit::commit_types())]
+    #[clap(name = "type", value_name = "TYPE", possible_values = cog_commit::commit_types())]
     typ: String,
 
     /// Commit description
@@ -193,16 +174,16 @@ struct CommitArgs {
     scope: Option<String>,
 
     /// Create a BREAKING CHANGE commit
-    #[structopt(short = "B", long)]
+    #[clap(short = 'B', long)]
     breaking_change: bool,
 
     /// Open commit message in an editor
-    #[structopt(short, long)]
+    #[clap(short, long)]
     edit: bool,
 }
 
 fn main() -> Result<()> {
-    let cli = Cli::from_args();
+    let cli = Cli::parse();
 
     match cli {
         Cli::Bump {
@@ -346,7 +327,7 @@ fn main() -> Result<()> {
             }
         }
         Cli::GenerateCompletions { shell } => {
-            Cli::clap().gen_completions_to("cog", shell, &mut std::io::stdout());
+            clap_complete::generate(shell, &mut Cli::into_app(), "cog", &mut std::io::stdout());
         }
         Cli::Commit(CommitArgs {
             typ,
