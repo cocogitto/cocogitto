66ab93e9791d1c41d97e9d17a0b77b26580def6f
darlaam <darlaam@pm.me>
2022-04-08 09:32:25 +0200

    fix: fix tests

--- tests/lib_tests/init.rs
+++ tests/lib_tests/init.rs
@@ -3,6 +3,7 @@ use std::path::Path;
 use crate::helpers::*;
 
 use anyhow::Result;
+use cmd_lib::run_cmd;
 use sealed_test::prelude::*;
 use speculoos::prelude::*;
 
@@ -30,6 +31,6 @@ fn should_skip_initialization_if_repository_exists() -> Result<()> {
     // Assert
     assert_that!(Path::new("cog.toml")).exists();
     assert_that!(git_log_head()?).is_equal_to("The first commit\n".to_string());
-    assert_that!(git_status()?).contains("new file:   cog.toml");
+    assert_that!(git_status()?).contains("git restore --staged");
     Ok(())
 }
