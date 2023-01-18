5c44dcd00192ff81c906f6cc3b56f64f74065930
Paul Delafosse <paul.delafosse@protonmail.com>
2023-01-18 16:34:01 +0100

    test(monorepo): add integration test

--- tests/lib_tests/bump.rs
+++ tests/lib_tests/bump.rs
@@ -1,6 +1,9 @@
 use anyhow::Result;
+use std::collections::HashMap;
+use std::path::PathBuf;
 
 use cmd_lib::run_cmd;
+use cocogitto::settings::{MonoRepoPackage, Settings};
 use cocogitto::{conventional::version::IncrementCommand, CocoGitto};
 use sealed_test::prelude::*;
 use speculoos::prelude::*;
@@ -24,6 +27,102 @@ fn bump_ok() -> Result<()> {
     // Assert
     assert_that!(result).is_ok();
     assert_latest_tag("1.1.0")?;
+    Ok(())
+}
+
+#[sealed_test]
+fn monorepo_bump_ok() -> Result<()> {
+    // Arrange
+    let mut settings = Settings {
+        ..Default::default()
+    };
+
+    init_monorepo(&mut settings)?;
+
+    let mut cocogitto = CocoGitto::get()?;
+
+    // Act
+    let result = cocogitto.create_monorepo_version(None, None, false);
+
+    // Assert
+    assert_that!(result).is_ok();
+    assert_tag_exists("0.1.0")?;
+    assert_tag_exists("one-0.1.0")?;
+    Ok(())
+}
+
+#[sealed_test]
+fn monorepo_with_tag_prefix_bump_ok() -> Result<()> {
+    // Arrange
+    let mut settings = Settings {
+        tag_prefix: Some("v".to_string()),
+        ..Default::default()
+    };
+
+    init_monorepo(&mut settings)?;
+
+    let mut cocogitto = CocoGitto::get()?;
+
+    // Act
+    let result = cocogitto.create_monorepo_version(None, None, false);
+
+    // Assert
+    assert_that!(result).is_ok();
+    assert_tag_exists("v0.1.0")?;
+    assert_tag_exists("one-v0.1.0")?;
+    Ok(())
+}
+
+#[sealed_test]
+fn package_bump_ok() -> Result<()> {
+    // Arrange
+    let mut settings = Settings {
+        ..Default::default()
+    };
+
+    init_monorepo(&mut settings)?;
+    let package = settings.packages.get("one").unwrap();
+    let mut cocogitto = CocoGitto::get()?;
+
+    // Act
+    let result = cocogitto.create_package_version(
+        ("one", package),
+        IncrementCommand::AutoPackage("one".to_string()),
+        None,
+        None,
+        false,
+    );
+
+    // Assert
+    assert_that!(result).is_ok();
+    assert_tag_does_not_exist("0.1.0")?;
+    assert_tag_exists("one-0.1.0")?;
+    Ok(())
+}
+
+fn init_monorepo(settings: &mut Settings) -> Result<()> {
+    let mut packages = HashMap::new();
+    packages.insert(
+        "one".to_string(),
+        MonoRepoPackage {
+            path: PathBuf::from("one"),
+            ..Default::default()
+        },
+    );
+    settings.packages = packages;
+    let settings = toml::to_string(&settings)?;
+
+    git_init()?;
+    run_cmd!(
+        echo $settings > cog.toml;
+        git add .;
+        git commit -m "chore: first commit";
+        mkdir one;
+        echo "changes" > one/file;
+        git add .;
+        git commit -m "feat: package one feature";
+    )?;
+
     Ok(())
 }
 
