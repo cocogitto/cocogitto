c2912bd5e02ccf7b80eb6ac714e6fb14a34defa1
Sanchith Hegde <sanchith.hegde01@gmail.com>
2023-05-20 22:09:39 +0530

    test(check): add CLI tests for running check on commit range

--- tests/cog_tests/check.rs
+++ tests/cog_tests/check.rs
@@ -83,3 +83,63 @@ fn cog_check_from_latest_tag_failure() -> Result<()> {
         .stderr(predicate::str::contains("Found 1 non compliant commits"));
     Ok(())
 }
+
+#[sealed_test]
+fn cog_check_commit_range_ok() -> Result<()> {
+    // Arrange
+    git_init()?;
+    let range_start = git_commit("chore: init")?;
+    git_commit("feat: feature")?;
+    let range_end = git_commit("fix: bug fix")?;
+    let range = format!("{range_start}..{range_end}");
+
+    // Act
+    Command::cargo_bin("cog")?
+        .arg("check")
+        .arg(range)
+        // Assert
+        .assert()
+        .success()
+        .stderr(predicate::str::contains("No errored commits"));
+    Ok(())
+}
+
+#[sealed_test]
+fn cog_check_commit_range_failure() -> Result<()> {
+    // Arrange
+    git_init()?;
+    let range_start = git_commit("chore: init")?;
+    git_commit("toto: errored commit")?;
+    git_commit("feat: feature")?;
+    git_commit("fix: bug fix")?;
+    let range_end = git_commit("toto: africa")?;
+    let range = format!("{range_start}..{range_end}");
+
+    // Act
+    Command::cargo_bin("cog")?
+        .arg("check")
+        .arg(range)
+        // Assert
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains("Found 2 non compliant commits"));
+    Ok(())
+}
+
+#[sealed_test]
+fn cog_check_from_latest_tag_and_commit_range_failure() -> Result<()> {
+    // Arrange
+
+    // Act
+    Command::cargo_bin("cog")?
+        .arg("check")
+        .arg("--from-latest-tag")
+        .arg("abcdef..fedcba")
+        // Assert
+        .assert()
+        .failure()
+        .stderr(predicate::str::contains(
+            "the argument '--from-latest-tag' cannot be used with '[RANGE]'",
+        ));
+    Ok(())
+}
