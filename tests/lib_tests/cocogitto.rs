67d0de2b50fb0d57c210185aa2565e9215f7df19
Sanchith Hegde <sanchith.hegde01@gmail.com>
2023-05-20 20:26:32 +0530

    test(check): add tests for running check on commit range

--- tests/lib_tests/cocogitto.rs
+++ tests/lib_tests/cocogitto.rs
@@ -157,3 +157,79 @@ fn long_commit_summary_does_not_panic() -> Result<()> {
     assert_that!(check.is_ok());
     Ok(())
 }
+
+#[sealed_test]
+fn check_commit_ok_commit_range() -> Result<()> {
+    // Arrange
+    git_init()?;
+    let range_start = git_commit("feat: a valid commit")?;
+    let range_end = git_commit("chore(test): another valid commit")?;
+    let range = format!("{range_start}..{range_end}");
+    let cocogitto = CocoGitto::get()?;
+
+    // Act
+    let check = cocogitto.check(true, false, Some(range));
+
+    // Assert
+    assert_that!(check).is_ok();
+    Ok(())
+}
+
+#[sealed_test]
+fn check_commit_err_commit_range() -> Result<()> {
+    // Arrange
+    git_init_and_set_current_path("commit_history_err")?;
+    create_empty_config()?;
+    let range_start = git_commit("feat: a valid commit")?;
+    let range_end = git_commit("errored commit")?;
+    let range = format!("{range_start}..{range_end}");
+    let cocogitto = CocoGitto::get()?;
+
+    // Act
+    let check = cocogitto.check(true, false, Some(range));
+
+    // Assert
+    assert_that!(check).is_err();
+    Ok(())
+}
+
+#[sealed_test]
+fn check_commit_range_err_with_merge_commit() -> Result<()> {
+    // Arrange
+    git_init()?;
+    let range_start = git_commit("feat: a valid commit")?;
+    let range_end = git_commit("Merge feature one into main")?;
+    let range = format!("{range_start}..{range_end}");
+    let cocogitto = CocoGitto::get()?;
+
+    // Act
+    let check = cocogitto.check(false, false, Some(range));
+
+    // Assert
+    assert_that!(check).is_err();
+    Ok(())
+}
+
+#[sealed_test]
+fn check_commit_range_ok_with_merge_commit_ignored() -> Result<()> {
+    // Arrange
+    git_init()?;
+    let range_start = git_commit("feat: a valid commit")?;
+    run_cmd!(git checkout -b branch;)?;
+    git_commit("feat: commit on another branch")?;
+    run_cmd!(
+        git checkout -;
+        git merge branch --no-ff;
+        git --no-pager log;
+    )?;
+    let range_end = git_commit("chore(test): another valid commit")?;
+    let range = format!("{range_start}..{range_end}");
+    let cocogitto = CocoGitto::get()?;
+
+    // Act
+    let check = cocogitto.check(false, true, Some(range));
+
+    // Assert
+    assert_that!(check).is_ok();
+    Ok(())
+}
