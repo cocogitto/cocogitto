b1ca37bbefdba3eaefa7a9139ef3fe4b4ac07f85
Paul Delafosse <paul.delafosse@protonmail.com>
2023-10-28 23:33:19 +0200

    ci: docker build

--- ci/aur_bump.sh
+++
@@ -1,8 +0,0 @@
-VERSION=$1
-
-cd aur/ || exit
-sed -i "s/pkgver=.*/pkgver=$VERSION/g" PKGBUILD
-updpkgsums
-makepkg --printsrcinfo > .SRCINFO
-git add .
-git commit -m "chore(aur): update aur package version to $VERSION"
