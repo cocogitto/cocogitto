3875f65918f2710b335a872a433b5cef0a7c82b6
Paul Delafosse <paul.delafosse@protonmail.com>
2024-05-15 12:29:37 +0200

    fix: populate monorepo changelog tera context

--- src/command/changelog.rs
+++ src/command/changelog.rs
@@ -1,7 +1,11 @@
 use crate::conventional::changelog::release::Release;
+use crate::conventional::changelog::template::MonoRepoContext;
+use crate::conventional::changelog::template::PackageBumpContext;
 use crate::conventional::changelog::template::Template;
 
+use crate::conventional::changelog::ReleaseType;
 use crate::CocoGitto;
+use crate::SETTINGS;
 use anyhow::anyhow;
 use anyhow::Result;
 
@@ -14,11 +18,53 @@ impl CocoGitto {
         Release::try_from(commit_range).map_err(Into::into)
     }
 
+    pub fn get_monorepo_changelog(&self, pattern: &str, template: Template) -> Result<String> {
+        let mut packages = vec![];
+
+        let package_data: Vec<(&String, String)> = SETTINGS
+            .packages
+            .iter()
+            .map(|(name, p)| (name, p.path.to_string_lossy().to_string()))
+            .collect();
+
+        for (package_name, package_path) in package_data.iter() {
+            let range = self
+                .repository
+                .get_commit_range_for_package(pattern, package_name)?;
+
+            let from = Some(range.from_oid());
+
+            let version = range.to_oid();
+
+            let context = PackageBumpContext {
+                package_name,
+                package_path,
+                version,
+                from,
+            };
+
+            packages.push(context);
+        }
+        let context = MonoRepoContext {
+            package_lock: false,
+            packages,
+        };
+
+        let commit_range = self
+            .repository
+            .get_commit_range_for_monorepo_global(pattern)?;
+
+        let changelog = Release::try_from(commit_range)?;
+        changelog
+            .into_markdown(template, ReleaseType::MonoRepo(context))
+            .map_err(Into::into)
+    }
+
     pub fn get_changelog_at_tag(&self, tag: &str, template: Template) -> Result<String> {
         let changelog = self.get_changelog(tag, false)?;
 
         changelog
-            .into_markdown(template)
+            .into_markdown(template, ReleaseType::Standard)
             .map_err(|err| anyhow!(err))
     }
 }
