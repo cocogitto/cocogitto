b20dd40e0022e7dc6103ef231d39bc89b17a6cd0
Paul Delafosse <paul.delafosse@protonmail.com>
2024-11-28 03:51:58 +0100

    chore: revert trustfall docgen

--- src/bin/cog-doc/main.rs
+++ src/bin/cog-doc/main.rs
@@ -1,19 +1,8 @@
-use crate::ser::Item;
 use clap::Parser;
 use cocogitto::settings::Settings;
-use items::FieldOrVariant;
-use query::STRUCT_QUERY;
 use schemars::schema_for;
-use std::collections::HashMap;
 use std::fs;
 use std::path::PathBuf;
-use std::process::Command;
-use trustfall_rustdoc::{VersionedIndexedCrate, VersionedRustdocAdapter};
-
-mod visitor;
-mod query;
-mod items;
-mod ser;
 
 const _CONFIG_REFERENCE_HEADING: &str = r#"# Configuration reference
 
@@ -34,11 +23,6 @@ pub enum Cli {
         #[arg(short, long)]
         out: Option<PathBuf>,
     },
-    ConfigReference {
-        /// Target output file
-        #[arg(short, long)]
-        out: Option<PathBuf>,
-    },
 }
 fn main() -> anyhow::Result<()> {
     let cli = Cli::parse();
@@ -52,58 +36,7 @@ fn main() -> anyhow::Result<()> {
                 Some(out) => fs::write(out, schema)?,
             }
         }
-        Cli::ConfigReference { .. } => {
-            trustfall_docgen();
-        }
     }
 
     Ok(())
 }
-
-fn trustfall_docgen() {
-    let manifest_dir = env!("CARGO_MANIFEST_DIR");
-    Command::new("cargo")
-        .env("RUSTC_BOOTSTRAP", "1")
-        .env(
-            "RUSTDOCFLAGS",
-            "-Z unstable-options --output-format=json --cap-lints=allow",
-        )
-        .arg("+nightly")
-        .arg("doc")
-        .arg("--no-deps")
-        .status().unwrap();
-
-    let json_doc = PathBuf::from(manifest_dir).join("target/doc/cocogitto.json");
-    let versioned_crate = trustfall_rustdoc::load_rustdoc(&json_doc).unwrap();
-    println!("{:?}", versioned_crate.crate_version());
-    let current = VersionedIndexedCrate::new(&versioned_crate);
-    let adapter = VersionedRustdocAdapter::new(&current, None).unwrap();
-
-    let result = query::trustfall_query(&adapter, "Settings", STRUCT_QUERY);
-    let items = visitor::visit_struct(&adapter, result);
-
-    let mut grouped_items: HashMap<String, Vec<FieldOrVariant>> = HashMap::new();
-
-    for item in items {
-        grouped_items.entry(match &item {
-            FieldOrVariant::Struct(s) => s.parent_name.clone(),
-            FieldOrVariant::Enum(e) => e.enum_name.clone()
-        })
-            .or_insert_with(Vec::new)
-            .push(item);
-    }
-
-    let grouped_items: Vec<Item> = grouped_items.into_iter()
-        .map(|(name, value)| Item {
-            name,
-            values: value,
-        })
-        .collect();
-
-    for item in grouped_items.iter() {
-        println!("{}", item.to_markdown());
-    }
-
-}
-
-
